03b4b72b0fb166fa7c20da830cb5dfa1d879105b
Sergey <Sergey@localhost.localdomain>
2022-12-20 23:24:47 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
+import os
 
 
-def download_text(url):
-    response = requests.get(url)
+def download_text(url, params):
+    response = requests.get(url, params=params)
     response.raise_for_status()
     return response.text
 
@@ -15,9 +16,15 @@ def download_file(url):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    url = "https://tululu.org/txt.php?id=32168"
-    text = download_text(url)
-    with open('book.txt', 'w') as file:
-        file.write(text)
+    books_dir = 'books'
+    os.makedirs(books_dir, exist_ok=True)
+
+    base_url = "https://tululu.org/txt.php"
+    for book_id in range(0, 10):
+        params = {'id': book_id}
+        book_path = os.path.join(books_dir, f'id{book_id}.txt')
+        text = download_text(base_url, params)
+        with open(book_path, 'w') as file:
+            file.write(text)
 
 
