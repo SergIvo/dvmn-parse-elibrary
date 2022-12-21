fd989d5ce45e848d64c347eb747a7247f5ed0141
Sergey <Sergey@localhost.localdomain>
2022-12-21 23:31:13 +0300

    Main function downloads books with proper filenames

--- main.py
+++ main.py
@@ -11,11 +11,8 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_text(url, params=None):
-    if params:
-        response = requests.get(url, params=params)
-    else:
-        response = requests.get(url)
+def download_text(url):
+    response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     return response.text
@@ -30,12 +27,13 @@ def download_file(url):
 def get_title_and_author(url):
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
     author_and_title_tag = soup.find('div', attrs={'id': 'content'}).find('h1')
     title, _ = author_and_title_tag.text.split('::')
     title = title.strip()
     author = author_and_title_tag.a.text
-    print(author, title)
+    return title, author
 
 
 def download_txt(url, filename, folder='books'):
@@ -51,23 +49,25 @@ def main():
     books_dir = 'books'
     os.makedirs(books_dir, exist_ok=True)
 
-    base_url = "https://tululu.org/txt.php"
+    base_book_url = "https://tululu.org/b{}/"
+    base_text_url = "https://tululu.org/txt.php?id={}"
     for book_id in range(1, 11):
-        params = {'id': book_id}
         try:
-            text = download_text(base_url, params)
-            book_path = os.path.join(books_dir, f'id{book_id}.txt')
-            with open(book_path, 'w') as file:
-                file.write(text)
+            book_url = base_book_url.format(book_id)
+            title, _ = get_title_and_author(book_url)
+            filename = f'{book_id}. {title}'
+        except requests.HTTPError:
+            print("Requested book doesn't exist.")
+            continue
+        try:
+            text_url = base_text_url.format(book_id)
+            text = download_txt(text_url, filename, books_dir)
         except requests.HTTPError:
             print("Requested book doesn't exist.")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    url = 'https://tululu.org/txt.php?id=1'
-    filepath = download_txt(url, 'Али/би', folder='books\\')
-    print(filepath)
-    filepath = download_txt(url, 'Али\\би', folder='txt\\')
-    print(filepath)
+    main()
+
 
