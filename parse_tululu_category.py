2e16dcc708da920b46fb394d57fe425b7ce807a1
Sergey <Sergey@localhost.localdomain>
2022-12-27 23:43:23 +0300

    Parse all book urls from a single sci-fi page

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,17 +4,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
+
+
+
 def main():
     base_url = 'https://tululu.org/l55/'
     response = requests.get(base_url)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
-    book_url_tag = soup.find('table', attrs={'class': 'd_book'}).find('a')
-    relative_book_url = book_url_tag['href']
+    book_tags = soup.find_all('table', attrs={'class': 'd_book'})
 
-    book_url = urljoin(base_url, relative_book_url)
-    print(book_url)
+    book_urls = []
+    for tag in book_tags:
+        relative_book_url = tag.find('a')['href']
+        book_url = urljoin(base_url, relative_book_url)
+        print(book_url)
+
+        book_urls.append(book_url)
+
+    print(len(book_urls))
 
 
 if __name__ == '__main__':
