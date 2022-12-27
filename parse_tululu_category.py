36aea47ef4e8bd3d25a2401169a6158cedbddab7
Sergey <Sergey@localhost.localdomain>
2022-12-27 23:59:42 +0300

    Parse all book urls from ten pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,12 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
-
-
-def main():
-    base_url = 'https://tululu.org/l55/'
-    response = requests.get(base_url)
+def parse_urls_from_page(page_url):
+    response = requests.get(page_url)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
@@ -18,10 +14,23 @@ def main():
     book_urls = []
     for tag in book_tags:
         relative_book_url = tag.find('a')['href']
-        book_url = urljoin(base_url, relative_book_url)
+        book_url = urljoin(page_url, relative_book_url)
         print(book_url)
 
         book_urls.append(book_url)
+
+    return book_urls
+
+
+def main():
+    base_url = 'https://tululu.org/l55/{}/'
+
+    book_urls = []
+    for page_number in range(1, 11):
+        page_url = base_url.format(page_number)
+        url_from_page = parse_urls_from_page(page_url)
+
+        book_urls.extend(url_from_page)
 
     print(len(book_urls))
 
