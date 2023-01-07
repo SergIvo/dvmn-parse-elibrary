6a26a24ad0286c757d2c6443495a582efd3453c7
Sergey <Sergey@localhost.localdomain>
2023-01-07 03:02:48 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -3,30 +3,34 @@ import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-media_directory = 'media'
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-with open(os.path.join(media_directory, 'books.json'), 'r') as json_file:
-    books_details = json.load(json_file)
+    media_directory = 'media'
+    with open(os.path.join(media_directory, 'books.json'), 'r') as json_file:
+        books_details = json.load(json_file)
 
-for book in books_details:
-    image_real_name = ''.join(book['image_relative_url'].split('/'))
-    image_path = os.path.join(media_directory, 'images', image_real_name)
-    book['image_relative_url'] = image_path
+    for book in books_details:
+        image_real_name = ''.join(book['image_relative_url'].split('/'))
+        image_path = os.path.join(media_directory, 'images', image_real_name)
+        book['image_relative_url'] = image_path
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
+    rendered_page = template.render(book_cards=books_details)
 
-rendered_page = template.render(
-    book_cards=books_details
-)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    on_reload()
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
